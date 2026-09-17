fcab3ac7b70e5494e835bab28d7d247809c3abc3
José Augusto <10750142@mackenzista.com.br>
2026-09-17 23:22:13 +0000

    suB1

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App{
     public static void main(String[] args) throws Exception {
-        
+        wget https://repo1.maven.org/maven2/org/postgresql/postgresql/42.7.5/postgresql-42.7.5.jar
     }
 }
